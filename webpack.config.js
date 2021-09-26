be91859e6f17632df7419436e4e2eb9dbfea478f
Rafael <37466798+likeavenus@users.noreply.github.com>
2021-09-26 13:22:21 +0300

    add cross-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
+        clean: true,
         filename: 'main.js',
         path: path.resolve(__dirname, 'build'),
     },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
 };
